12898913c5383300a9078ecce173b7299278c541
Justin Makeig <jmpublic+github@makeig.com>
2021-12-31 22:39:41 +0000

    Kinda plumbed through.

--- src/routes/child.svelte
+++ src/routes/child.svelte
@@ -2,6 +2,58 @@
 	export let duration;
 	export let elapsed;
 
+	import { createMachine, interpret } from "xstate";
+	const machine = createMachine({
+		id: "child",
+		initial: "idle",
+		states: {
+			idle: {
+				on: {
+					start: {
+						target: "running"
+					}
+				}
+			},
+			running: {
+				after: {
+					2500: {
+						target: "done"
+					}
+				}
+			},
+			done: {
+				type: "final"
+			}
+		}
+	});
+
+	const service = interpret(
+		machine
+		//.withConfig(additionalConfig)
+		//.withContext({ ...machine.context, ...initialContext })
+	);
+
+	import { createEventDispatcher } from "svelte";
+	const fire = createEventDispatcher();
+
+	import { readable } from "svelte/store";
+	const status = readable(machine.initialState, (set) => {
+		service
+			.onTransition((state) => {
+				if (false !== state.changed) {
+					set(state);
+				}
+			})
+			.onDone((state) => fire("done"))
+			.start();
+		return () => service.stop();
+	});
+
+	import { onMount } from "svelte";
+	onMount(() => {
+		service.send("start");
+	});
+	/*
 	export function start(handler) {
 		const interval = 0.1 * 1000;
 		const timeout = setInterval(() => {
@@ -14,14 +66,9 @@
 		}, interval);
 		return () => clearInterval(timeout);
 	}
+	*/
 </script>
 
 <div style="background: #eee; padding: 0.5em;">
-	<button on:click={() => start(() => console.log("Completed timer"))}>
-		Start timer
-	</button>
-	<ul>
-		<li>Duration: {duration}</li>
-		<li>Elpased: {elapsed}</li>
-	</ul>
+	{$status.value}
 </div>
--- src/routes/parent.svelte
+++ src/routes/parent.svelte
@@ -1,19 +1,73 @@
 <script>
-	import { createMachine } from "xstate";
-
-	import Child from "./child.svelte"
-	let child;
-	let status = "uninitialized";
-
-	const service = {
-		send(message) {
-			child.start(() => (status = "timer done"));
+	import { createMachine, interpret, assign, matchesState } from "xstate";
+	const machine = createMachine({
+		id: "parent",
+		context: {
+			current: null
+		},
+		initial: "idle",
+		states: {
+			idle: {
+				on: {
+					start: {
+						target: "next"
+					}
+				}
+			},
+			next: {
+				always: [
+					{
+						target: "exercising",
+						actions: assign({
+							current: (context, event) =>
+								null === context.current ? 0 : context.current + 1
+						}),
+						cond: (context, event) => context.current < 5
+					},
+					{ target: "done" }
+				]
+			},
+			exercising: {
+				// do timer
+			},
+			done: {
+				type: "final"
+			}
 		}
-	};
+	});
+
+	const service = interpret(
+		machine
+		//.withConfig(additionalConfig)
+		//.withContext({ ...machine.context, ...initialContext })
+	);
+
+	import { createEventDispatcher } from "svelte";
+	const fire = createEventDispatcher();
+
+	import { readable } from "svelte/store";
+	const status = readable(machine.initialState, (set) => {
+		service
+			.onTransition((state) => {
+				if (false !== state.changed) {
+					set(state);
+				}
+			})
+			.onDone((state) => console.log("Parent done"))
+			.start();
+		return () => service.stop();
+	});
+
+	import { onMount } from "svelte";
+	onMount(() => {
+		service.send("start");
+	});
+
+	import Child from "./child.svelte";
 </script>
 
-<h1>
-	Parent: {status}
-</h1>
-<button on:click={(evt) => service.send("start")}> Send </button>
-<Child duration={5} elapsed={0} bind:this={child} />
+<h1>Parent</h1>
+
+{#if $status.matches("exercising")}
+	<Child duration={5} elapsed={0} on:done={(evt) => console.log(evt)} />
+{/if}
